f09a459ca287e072be74ea65fde638b8cf1dfb1b
BigBlobby <tom_dempster@hotmail.co.uk>
2024-05-19 16:17:00 +0100

    fix email confirmation flow

--- src/lib/config/app-config.ts
+++ src/lib/config/app-config.ts
@@ -19,14 +19,14 @@ interface IAppConfig {
 
 // This is where the various config options sit
 export const AppConfig: IAppConfig = {
-  trialPeriodDays: 7, // Change to control trial period length
+  trialPeriodDays: 0, // Change to control trial period length
   trialPeriodCollectCard: false, // Choose whether to collect credit card details or not on checkout for initial trial period
   auth: {
     allowOauth: true, // Allow users to sign in with 0auth
     allowEmail: true, // Allow users to sign in via magic link (at least one of the two options allowEmail/allowPassword must be true)
     allowPassword: true, // Allow users to sign in using a password (at least one of the two options allowEmail/allowPassword must be true)
   },
-  payments: 'paddle',
+  payments: 'stripe',
   stripe: {
     useEmbeddedCheckout: true, // Show the checkout on the site itself, rather than going to a stripe hosted page
   },
