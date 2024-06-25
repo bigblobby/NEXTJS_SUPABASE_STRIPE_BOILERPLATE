49a4ce7cc9b0a017eb0b52c66e06528cdefdaa55
BigBlobby <tom_dempster@hotmail.co.uk>
2024-06-25 22:26:59 +0100

    Add framer motion

--- src/app/(public)/(homepage)/page-content.tsx
+++ src/app/(public)/(homepage)/page-content.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Pricing from '@/lib/components/pricing';
 import { User } from '@supabase/supabase-js';
 import CTA from '@/lib/components/cta';
@@ -7,6 +9,7 @@ import Newsletter from '@/lib/components/newsletter';
 import { LsSubscription, PaddleSubscription, Subscription } from '@/lib/types/supabase/table.types';
 import TestimonialLarge from '@/lib/components/testimonial-large';
 import FeatureTabs from '@/lib/components/feature-tabs';
+import { motion, Variants } from "framer-motion";
 
 interface HomepageContentProps {
   user: User | null | undefined;
@@ -14,6 +17,23 @@ interface HomepageContentProps {
   paddleSubscription: PaddleSubscription | null;
   lsSubscription: LsSubscription | null;
 }
+
+const sectionVariants: Variants = {
+  offscreen: {
+    y: 20,
+    opacity: 0,
+    scale: 0.9,
+  },
+  onscreen: {
+    y: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "tween",
+      duration: 0.2
+    }
+  }
+};
 
 export default function HomepageContent({
   user,
@@ -23,22 +43,74 @@ export default function HomepageContent({
 }: HomepageContentProps) {
   return (
     <>
-      <Hero />
-      <FeatureTabs />
-      <TestimonialLarge
-        name="John Doe"
-        title="CTO at Acme"
-        content={`"NextJS Boilerplate is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."`}
-      />
+      <motion.div
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <motion.div variants={sectionVariants}>
+          <Hero />
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <motion.div variants={sectionVariants}>
+          <FeatureTabs />
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <motion.div variants={sectionVariants}>
+          <TestimonialLarge
+            name="John Doe"
+            title="CTO at Acme"
+            content={`"NextJS Boilerplate is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."`}
+          />
+        </motion.div>
+      </motion.div>
+
       <Newsletter />
-      <Pricing
-        user={user}
-        subscription={subscription}
-        paddleSubscription={paddleSubscription}
-        lsSubscription={lsSubscription}
-      />
-      <FAQ />
-      <CTA />
+
+      <motion.div
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <motion.div variants={sectionVariants}>
+          <Pricing
+            user={user}
+            subscription={subscription}
+            paddleSubscription={paddleSubscription}
+            lsSubscription={lsSubscription}
+          />
+        </motion.div>
+      </motion.div>
+      <motion.div
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <motion.div variants={sectionVariants}>
+          <FAQ />
+        </motion.div>
+      </motion.div>
+      <motion.div
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <motion.div variants={sectionVariants}>
+          <CTA />
+        </motion.div>
+      </motion.div>
     </>
   )
 }
